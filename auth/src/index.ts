02a0dce2d1cec99157dab5a327f3209cad484920
ibrahim <ibrodex@gmail.com>
2022-02-27 06:41:23 +0100

    Skaffold Configuration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,16 @@ const app = express();
 
 app.use(json());
 
+
+
+app.get('/api/users/currentuser', (req, res) => {
+    res.send({
+        id: 1,
+        name: 'John Doe',
+        email: 'j@d.c'
+    });
+});
+
 app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-})
+    console.log('Listening on port 3000!!!!!!!');
+});
