c943b6ea4c1d5850695c4eb21996f4e4f41dee50
ibrahim <ibrodex@gmail.com>
2022-02-28 15:05:43 +0100

    User model implementation (146)

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 // check properties explicitly
 interface IUserAtrributes {
     email: string;
@@ -19,6 +20,14 @@ interface userDocument extends mongoose.Document {
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
+});
+
+userSchema.pre('save', async function(done){
+    if (this.isModified('password')){
+        const hashedPassword = await Password.toHash(this.get('password'));
+        this.set('password', hashedPassword);
+    }
+    done();
 });
 
 userSchema.statics.build  = (attributes: IUserAtrributes) => {
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 
 const router = express.Router();
@@ -14,7 +16,7 @@ router.post('/api/users/signup',[
         .trim()
         .isLength({min: 4, max: 20})
         .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res:Response) => {
+], async (req: Request, res:Response) => {
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -22,18 +24,29 @@ router.post('/api/users/signup',[
     }
     const {email, password} = req.body;
 
+    // checking if user exist with same email
+    const isUserExists = await User.findOne({email});
+
+    if(isUserExists){
+        throw new BadRequestError('User already exists');
+    }
+
+    const user = User.build({email, password});
+    await user.save();
+
+    res.status(201).send({message: 'User created successfully', data: user});
     console.log("Creating a user");
     // new users
 
 
 
 
-    throw new DatabaseConnectionError('Could not connect to the database');
-    res.send({
-        id: 1,
-        name: 'John Doe',
-        email: 'j@d.c'
-    });
+    // throw new DatabaseConnectionError('Could not connect to the database');
+    // res.send({
+    //     id: 1,
+    //     name: 'John Doe',
+    //     email: 'j@d.c'
+    // });
 });
 
 export { router as signupRouter };
