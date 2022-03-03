c9e98fb9b4626dc144961e896e3d11794ad93869
ibrahim <ibrodex@gmail.com>
2022-03-03 09:30:39 +0100

    User Creation bug fix

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -12,12 +12,13 @@ router.post('/api/users/signup',[
     body('email')
         .isEmail()
         .withMessage('Email must be valid'),
-    body('passsword')
+    body('password')
         .trim()
         .isLength({min: 4, max: 20})
         .withMessage('Password must be between 4 and 20 characters')
 ], async (req: Request, res:Response) => {
 
+   // console.log('here handler', req);
     const errors = validationResult(req);
     if(!errors.isEmpty()){
        throw new RequestValidationError(errors.array());
@@ -36,17 +37,7 @@ router.post('/api/users/signup',[
 
     res.status(201).send({message: 'User created successfully', data: user});
     console.log("Creating a user");
-    // new users
-
-
-
-
-    // throw new DatabaseConnectionError('Could not connect to the database');
-    // res.send({
-    //     id: 1,
-    //     name: 'John Doe',
-    //     email: 'j@d.c'
-    // });
+  
 });
 
 export { router as signupRouter };
